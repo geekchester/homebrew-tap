5e0445dd3f48bf7c7b543bd4f58b858e85857e32
Pavel <pavel.lavreshin@gmail.com>
2019-10-03 14:26:21 +0300

    Update flutter.rb

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -1,10 +1,17 @@
 cask "flutter" do
-  version "1.5.4-hotfix.2"
-  sha256 '6a2554c3754322848aa6b38e449c42d5aa9149ea82bf97f168782f68a8efa0b1'
+  version :latest
+  sha256 "8f7a5cfa1494b6758e1b9b7913871a60934c7f3d487979a6ef176ce4f2a7b4a3"
+  url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v1.9.1+hotfix.2-stable.zip"
 
-  url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v#{version}-stable.zip"
   name "flutter"
-  homepage "https://flutter.io"
+  homepage "https://flutter.dev"
 
   binary "flutter/bin/flutter", target: "flutter"
+  
+  postflight do
+    # Upgrade stable channel to the latest version
+    system_command '/usr/local/bin/flutter',
+                   args: ['upgrade', '--force'],
+                   sudo: false
+  end
 end
